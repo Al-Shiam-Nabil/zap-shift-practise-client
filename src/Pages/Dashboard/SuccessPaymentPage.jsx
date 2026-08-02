c99943bfdc8353341e4f5cae09a7fa5ee4b26b08
Al Shiam Nabil <nabil15-4777@diu.edu.bd>
2026-08-03 02:45:35 +0600

    tracking id added

--- src/Pages/Dashboard/SuccessPaymentPage.jsx
+++ src/Pages/Dashboard/SuccessPaymentPage.jsx
@@ -1,5 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSearchParams } from "react-router";
+import useAxiosSecure from "../../Hooks/useAxiosSecure";
 
 export default function SuccessPaymentPage() {
+  const [searchPrams] = useSearchParams();
+  const secureAxios = useAxiosSecure();
+
+  const sessionId = searchPrams.get("session_id");
+
+  useEffect(() => {
+    if (sessionId) {
+      secureAxios
+        .patch(`/payment-success?session_id=${sessionId}`)
+        .then((res) => console.log(res.data))
+        .catch((error) => console.log(error.message));
+    }
+  }, [sessionId, secureAxios]);
+
   return <div>SuccessPaymentPage</div>;
 }
